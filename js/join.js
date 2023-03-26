927943b9f3a6a0297a8c5b9a23d0e5edbe15476b
Inhoo Han <a5019999@naver.com>
2023-03-26 19:50:16 +0900

    확대했을 때 깨지는 부분 수정

--- js/join.js
+++ js/join.js
@@ -1,5 +1,5 @@
 $(function() {
-    // checkbox 전체선택 클릭
+    //checkbox 전체선택 클릭
     $('#checkbox .join_form_all').click(function() {
         if($('#all_cb').is(':checked')){
             $('.join_form_cb input').prop('checked', true);
@@ -8,7 +8,7 @@ $(function() {
         }
     });
 
-    // 선택 체크에 따른 전체선택 여부
+    //선택 체크에 따른 전체선택 여부
     $('.join_form_etc').click(function() {
         let total = $('input.join_form_input_cb').length;
         let checked = $('input.join_form_input_cb:checked').length;
@@ -20,5 +20,21 @@ $(function() {
         }
     });
 
-    $()
+    //비밀번호와 비밀번호 확인 동일여부 체크
+    $('#pw').keyup(function(){
+        $('#pw_notice').html('<br>');
+      });
+  
+      $('#pw_check').keyup(function(){
+  
+          if($('#pw').val() != $('#pw_check').val()){
+            $('#pw_notice').html('비밀번호 일치하지 않음<br>');
+            $('#pw_notice').attr('color', '#f82a2aa3');
+          } else{
+            $('#pw_notice').html('비밀번호 일치함<br>');
+            $('#pw_notice').attr('color', '#199894b3');
+          }
+  
+      });
+
 });
